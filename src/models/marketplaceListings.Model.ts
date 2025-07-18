f70d1f5670eb3b0a75fb9b9f4c96298bed58ceb4
KiranShahzadi-KS <kiranraja557@gmail.com>
2025-07-18 14:53:13 +0500

    update

---
+++ src/models/marketplaceListings.Model.ts
@@ -0,0 +1,72 @@
+import mongoose, { Schema, Document, model } from "mongoose";
+
+interface ILanguageTranslation {
+  locale: string;
+  translations: Record<string, any>;
+}
+
+export interface IMarketplaceListing extends Document {
+  leaser: mongoose.Schema.Types.ObjectId;
+  subCategory: mongoose.Types.ObjectId;
+  zone: mongoose.Types.ObjectId;
+  
+  ratings: {  
+    count: number;
+    average: number;
+  };
+  name?: string; 
+  images?: string[]; 
+  description: string;
+  address: string
+  currency?: string;
+  price: number;
+  isActive?: boolean;
+  language?: string;
+  languages?: ILanguageTranslation[];
+  [key: string]: any; 
+}
+
+const MarketplaceListingSchema = new Schema<IMarketplaceListing>(
+  {
+    leaser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subCategory: {
+      type: Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: true,
+    },
+    zone: {
+      type: Schema.Types.ObjectId,
+      ref: "Zone",
+      required: true,
+    },
+
+    ratings: {
+      count: { type: Number, default: 0 },
+      average: { type: Number, default: 0 },
+    },
+    name: { type: String }, 
+    images: [{ type: String }], 
+    description: { type: String, required: true },
+    address: { type: String, required: true },
+    currency: { type: String },
+    price: { type: Number, required: true },
+    isActive: { type: Boolean, default: true },
+    language: { type: String, default: "en" },
+    languages: [
+      {
+        locale: { type: String, required: true },
+        translations: { type: Schema.Types.Mixed },
+      },
+    ],
+  },
+  { timestamps: true, strict: false } // allows dynamic fields
+);
+
+export const MarketplaceListing = model<IMarketplaceListing>(
+  "MarketplaceListing",
+  MarketplaceListingSchema
+);
