19543381734669e9846d812fcd2122edd64162fb
KiranShahzadi-KS <kiranraja557@gmail.com>
2025-07-28 14:52:15 +0500

    feat: add order field to FAQ model and implement pagination for FAQ retrieval

--- src/models/faq.model.ts
+++ src/models/faq.model.ts
@@ -1,15 +1,16 @@
-// models/faq.model.ts
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IFaq extends Document {
   question: string;
   answer: string;
+  order: number; 
 }
 
 const faqSchema = new Schema<IFaq>(
   {
     question: { type: String, required: true },
     answer: { type: String, required: true },
+    order: { type: Number, required: true },
   },
   { timestamps: true }
 );
