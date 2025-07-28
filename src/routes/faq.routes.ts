e8448f21314d118f52b8709c07db459447054f3f
KiranShahzadi-KS <kiranraja557@gmail.com>
2025-07-28 16:04:26 +0500

    refactor: enhance FAQ model and routes with language translation handling

--- src/routes/faq.routes.ts
+++ src/routes/faq.routes.ts
@@ -7,13 +7,29 @@ import {
   deleteFAQ,
 } from "../controllers/faq.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { languageTranslationMiddleware } from "../middlewares/languageTranslation.middleware";
+import { FAQ } from "../models/faq.model";
 
 const router = express.Router();
+
+function asyncHandler(fn: any) {
+  return function (req: any, res: any, next: any) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
 
 router.post("/", authMiddleware, createFAQ);
 router.get("/", getAllFAQs);
 router.get("/:id", getFAQById);
-router.patch("/:id", authMiddleware, updateFAQ);
+
+
+router.patch(
+  "/:id",
+  authMiddleware,
+  asyncHandler(languageTranslationMiddleware(FAQ)),
+  updateFAQ
+);
+
 router.delete("/:id", authMiddleware, deleteFAQ);
 
 export default router;
