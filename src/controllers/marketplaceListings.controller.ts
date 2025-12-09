784b73485733ba888f6a176cd1e24ca7bdfb8c86
KiranShahzadi-KS <kiranraja557@gmail.com>
2025-12-09 15:18:25 +0500

    feat: enhance getAllMarketplaceListings and getMarketplaceListingByIdforLeaser with , admin fee, and tax calculations - clean code

--- src/controllers/marketplaceListings.controller.ts
+++ src/controllers/marketplaceListings.controller.ts
@@ -472,235 +472,6 @@ export const getAllMarketplaceListingsforLeaser = async (
   }
 };
 
-//UPDATED getAllMarketplaceListings — added reviews & average rating
-// export const getAllMarketplaceListings = async (
-//   req: AuthRequest,
-//   res: Response,
-//   next: NextFunction
-// ): Promise<void> => {
-//   const session = await mongoose.startSession();
-//   session.startTransaction();
-
-//   try {
-//     const locale = req.headers["language"]?.toString()?.toLowerCase() || "en";
-//     const {
-//       page = 1,
-//       limit = 10,
-//       zone,
-//       subCategory,
-//       category,
-//       all,
-//       recent,
-//       minPrice,
-//       maxPrice,
-//     } = req.query;
-
-//     const filter: any = {};
-
-//     //ROLE-BASED FILTERS
-//     if (req.user) {
-//       if (req.user.role === "admin") {
-//         if (zone && mongoose.Types.ObjectId.isValid(String(zone))) {
-//           filter.zone = new mongoose.Types.ObjectId(String(zone));
-//         }
-//       } else {
-//         if (all === "true") {
-//           if (zone && mongoose.Types.ObjectId.isValid(String(zone))) {
-//             filter.zone = new mongoose.Types.ObjectId(String(zone));
-//           }
-//         } else {
-//           filter.leaser = new mongoose.Types.ObjectId(String(req.user.id));
-//           if (zone && mongoose.Types.ObjectId.isValid(String(zone))) {
-//             filter.zone = new mongoose.Types.ObjectId(String(zone));
-//           }
-//         }
-//       }
-//     } else {
-//       if (zone && mongoose.Types.ObjectId.isValid(String(zone))) {
-//         filter.zone = new mongoose.Types.ObjectId(String(zone));
-//       }
-//     }
-
-//     //CATEGORY & SUBCATEGORY FILTERS
-//     if (subCategory && mongoose.Types.ObjectId.isValid(String(subCategory))) {
-//       filter.subCategory = new mongoose.Types.ObjectId(String(subCategory));
-//     }
-
-//     if (category && mongoose.Types.ObjectId.isValid(String(category))) {
-//       const subCategoryIds = await SubCategory.find({
-//         category: category,
-//       }).distinct("_id");
-//       filter.subCategory = { $in: subCategoryIds };
-//     }
-
-
-//     // add price filter 
-//     if (minPrice || maxPrice) {
-//       filter.price = {}; 
-
-//       if (minPrice) {
-//         filter.price.$gte = Number(minPrice);
-//       }
-
-//       if (maxPrice) {
-//         filter.price.$lte = Number(maxPrice); 
-//       }
-//     }
-
-//     //AGGREGATION PIPELINE
-//     const pipeline: any[] = [
-//       { $match: filter },
-//       {
-//         $lookup: {
-//           from: "categories",
-//           localField: "subCategory",
-//           foreignField: "_id",
-//           as: "subCategory",
-//         },
-//       },
-//       { $unwind: "$subCategory" },
-//       {
-//         $lookup: {
-//           from: "users",
-//           localField: "leaser",
-//           foreignField: "_id",
-//           as: "leaser",
-//         },
-//       },
-//       { $unwind: "$leaser" },
-//       {
-//         $lookup: {
-//           from: "zones",
-//           localField: "zone",
-//           foreignField: "_id",
-//           as: "zone",
-//         },
-//       },
-//       { $unwind: "$zone" },
-
-//       // Lookup all bookings for each listing
-//       {
-//         $lookup: {
-//           from: "bookings",
-//           localField: "_id",
-//           foreignField: "marketplaceListingId",
-//           as: "bookings",
-//         },
-//       },
-
-//       //Lookup reviews for those bookings
-//       {
-//         $lookup: {
-//           from: "reviews",
-//           localField: "bookings._id",
-//           foreignField: "bookingId",
-//           as: "reviews",
-//         },
-//       },
-
-//       //Calculate average rating
-//       {
-//         $addFields: {
-//           averageRating: { $avg: "$reviews.stars" },
-//           totalReviews: { $size: "$reviews" },
-//         },
-//       },
-
-//       // Lookup Form based on subCategory + zone
-//       {
-//         $lookup: {
-//           from: "forms",
-//           let: { subCatId: "$subCategory._id", zoneId: "$zone._id" },
-//           pipeline: [
-//             {
-//               $match: {
-//                 $expr: {
-//                   $and: [
-//                     { $eq: ["$subCategory", "$$subCatId"] },
-//                     { $eq: ["$zone", "$$zoneId"] },
-//                   ],
-//                 },
-//               },
-//             },
-//             {
-//               $project: {
-//                 userDocuments: 1,
-//                 leaserDocuments: 1,
-//               },
-//             },
-//           ],
-//           as: "form",
-//         },
-//       },
-//       {
-//         $addFields: {
-//           userDocuments: {
-//             $ifNull: [{ $arrayElemAt: ["$form.userDocuments", 0] }, []],
-//           },
-//           leaserDocuments: {
-//             $ifNull: [{ $arrayElemAt: ["$form.leaserDocuments", 0] }, []],
-//           },
-//         },
-//       },
-//       { $project: { form: 0 } },
-//       { $skip: (Number(page) - 1) * Number(limit) },
-//       { $limit: Number(limit) },
-//     ];
-
-//     //Sort by recently posted date (createdAt)
-//     if (recent === "true") {
-//       pipeline.push({ $sort: { createdAt: -1 } });
-//     }
-
-//     const listings = await MarketplaceListing.aggregate(pipeline).session(
-//       session
-//     );
-//     const total = await MarketplaceListing.countDocuments(filter).session(
-//       session
-//     );
-
-//     //LANGUAGE HANDLING
-//     const final = listings.map((obj: any) => {
-//       const listingLang = obj.languages?.find((l: any) => l.locale === locale);
-//       if (listingLang?.translations) {
-//         obj.description =
-//           listingLang.translations.description || obj.description;
-//       }
-//       delete obj.languages;
-//       return obj;
-//     });
-
-//     //STATS
-//     const uniqueUserIds = await MarketplaceListing.distinct(
-//       "leaser",
-//       filter
-//     ).session(session);
-//     const totalUsersWithListings = uniqueUserIds.length;
-
-//     await session.commitTransaction();
-//     session.endSession();
-
-//     sendResponse(
-//       res,
-//       {
-//         listings: final,
-//         total,
-//         page: +page,
-//         limit: +limit,
-//         totalUsersWithListings,
-//         totalMarketplaceListings: total,
-//       },
-//       `Fetched listings${locale !== "en" ? ` (locale: ${locale})` : ""}`,
-//       STATUS_CODES.OK
-//     );
-//   } catch (err) {
-//     await session.abortTransaction();
-//     session.endSession();
-//     next(err);
-//   }
-// };
-
-
 // UPDATED getAllMarketplaceListings — added reviews, average rating, adminFee & tax
 export const getAllMarketplaceListings = async (
   req: AuthRequest,
@@ -925,8 +696,8 @@ export const getAllMarketplaceListings = async (
   }
 };
 
-
-// UPDATED getMarketplaceListingByIdforLeaser — include all reviews & average rating
+// UPDATED getMarketplaceListingByIdforLeaser
+//adminFee & tax added using SAME logic as booking & getAllMarketplaceListings
 export const getMarketplaceListingByIdforLeaser = async (
   req: AuthRequest,
   res: Response,
@@ -934,6 +705,7 @@ export const getMarketplaceListingByIdforLeaser = async (
 ): Promise<void> => {
   const session = await mongoose.startSession();
   session.startTransaction();
+
   try {
     const { id } = req.params;
     const locale = req.headers["language"]?.toString()?.toLowerCase() || "en";
@@ -945,9 +717,10 @@ export const getMarketplaceListingByIdforLeaser = async (
       return;
     }
 
-    const doc = await MarketplaceListing.findById(id)
+    const doc: any = await MarketplaceListing.findById(id)
       .populate({ path: "subCategory", populate: { path: "category" } })
       .populate("leaser")
+      .populate("zone") 
       .lean();
 
     if (!doc) {
@@ -957,58 +730,78 @@ export const getMarketplaceListingByIdforLeaser = async (
       return;
     }
 
-    //Fetch all bookings related to this listing
-    const bookings = await Booking.find({ marketplaceListingId: id }).select(
-      "_id"
-    );
-
-    //Fetch all reviews related to those bookings
+    const bookings = await Booking.find({ marketplaceListingId: id }).select("_id");
     const bookingIds = bookings.map((b) => b._id);
+
     const reviews = await Review.find({ bookingId: { $in: bookingIds } })
       .populate("userId", "name email")
       .lean();
 
-    //Calculate average rating
     const averageRating =
       reviews.length > 0
         ? reviews.reduce((sum, r) => sum + r.stars, 0) / reviews.length
         : 0;
 
-    // Add reviews and average to the response
-    (doc as any).reviews = reviews;
-    (doc as any).averageRating = Number(averageRating.toFixed(2));
-
-    //Existing cleanup
-    delete (doc as any).zone;
-    delete (doc as any).documents;
-    delete (doc as any).__v;
-    delete (doc as any).languages;
-
-    if (doc.leaser) {
-      delete (doc.leaser as any).documents;
-      delete (doc.leaser as any).password;
-      delete (doc.leaser as any).otp;
-      delete (doc.leaser as any).stripe;
-      delete (doc.leaser as any).__v;
-    }
+    doc.reviews = reviews;
+    doc.averageRating = Number(averageRating.toFixed(2));
 
     const form = await Form.findOne({
-      subCategory: doc.subCategory?._id || doc.subCategory,
-      zone: doc.zone,
+      subCategory: doc.subCategory?._id,
+      zone: doc.zone?._id,
     })
-      .select("userDocuments leaserDocuments")
+      .select("setting userDocuments leaserDocuments")
       .session(session)
       .lean();
 
     if (form) {
-      (doc as any).userDocuments = form.userDocuments || [];
-      (doc as any).leaserDocuments = form.leaserDocuments || [];
+      doc.userDocuments = form.userDocuments || [];
+      doc.leaserDocuments = form.leaserDocuments || [];
+    } else {
+      doc.userDocuments = [];
+      doc.leaserDocuments = [];
+    }
+
+    if (form?.setting) {
+      const renterCommission =
+        (form.setting.renterCommission?.value || 0) / 100;
+      const leaserCommission =
+        (form.setting.leaserCommission?.value || 0) / 100;
+      const taxRate = (form.setting.tax || 0) / 100;
+
+      const totalCommissionRate = renterCommission + leaserCommission;
+
+      const adminFee = doc.price * totalCommissionRate;
+      const tax = (doc.price + adminFee) * taxRate;
+
+      doc.adminFee = Number(adminFee.toFixed(2));
+      doc.tax = Number(tax.toFixed(2));
+    } else {
+      doc.adminFee = 0;
+      doc.tax = 0;
+    }
+
+    delete doc.documents;
+    delete doc.languages;
+    delete doc.__v;
+
+    if (doc.leaser) {
+      delete doc.leaser.documents;
+      delete doc.leaser.password;
+      delete doc.leaser.otp;
+      delete doc.leaser.stripe;
+      delete doc.leaser.__v;
+    }
+
+
+    const listingLang = doc.languages?.find((l: any) => l.locale === locale);
+    if (listingLang?.translations) {
+      doc.description = listingLang.translations.description || doc.description;
     }
 
     await session.commitTransaction();
     session.endSession();
 
-    sendResponse(res, doc, "Listing fetched with reviews", STATUS_CODES.OK);
+    sendResponse(res, doc, "Listing fetched successfully", STATUS_CODES.OK);
   } catch (err) {
     await session.abortTransaction();
     session.endSession();
