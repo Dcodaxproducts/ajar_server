eaec898f6890f6defacd3b763ce770314b35bd22
umerwe <uemyy1@gmail.com>
2025-12-23 09:49:51 +0500

    updated

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -5,71 +5,114 @@ import stripe from "../utils/stripe";
 import mongoose from "mongoose";
 import { Request, Response } from "express";
 import { sendNotification } from "../utils/notifications";
+import { AuthRequest } from "../middlewares/auth.middleware";
+import { WalletTransaction } from "../models/walletTransaction.model";
 
 // CREATE PAYMENT INTENT
-export const createBookingPayment = async (req: Request, res: Response) => {
+export const createBookingPayment = async (req: AuthRequest, res: Response) => {
   try {
-    const { bookingId } = req.body;
+    const { bookingId, userAmount } = req.body;
+    const userData = req?.user;
 
-    console.log(" [PAYMENT INIT] Booking ID:", bookingId);
+    if (bookingId) {
+      console.log(" [PAYMENT INIT] Booking ID:", bookingId);
 
-    if (!mongoose.Types.ObjectId.isValid(bookingId)) {
-      return res.status(400).json({ message: "Invalid booking ID" });
-    }
+      if (!mongoose.Types.ObjectId.isValid(bookingId)) {
+        return res.status(400).json({ message: "Invalid booking ID" });
+      }
 
-    const booking = await Booking.findById(bookingId).populate("renter");
-    if (!booking) {
-      return res.status(404).json({ message: "Booking not found" });
-    }
+      const booking = await Booking.findById(bookingId).populate("renter");
+      if (!booking) {
+        return res.status(404).json({ message: "Booking not found" });
+      }
 
-    const user = booking.renter as any;
-    if (!user?.stripe?.customerId) {
-      return res
-        .status(400)
-        .json({ message: "Stripe customer not found for user" });
-    }
+      const user = booking.renter as any;
+      if (!user?.stripe?.customerId) {
+        return res
+          .status(400)
+          .json({ message: "Stripe customer not found for user" });
+      }
 
-    // FIX: Price is already calculated in booking controller
-    const amount = booking.priceDetails.totalPrice;
-    const amountInCents = Math.round(amount * 100);
+      // FIX: Price is already calculated in booking controller
+      const amount = booking.priceDetails.totalPrice;
+      const amountInCents = Math.round(amount * 100);
 
-    console.log("Charging EXACT booking total:", amount);
+      console.log("Charging EXACT booking total:", amount);
 
 
-    if (!amount || amountInCents < 50) {
-      return res.status(400).json({
-        message: "Booking amount must be at least $0.50",
+      if (!amount || amountInCents < 50) {
+        return res.status(400).json({
+          message: "Booking amount must be at least $0.50",
+          amount,
+        });
+      }
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: Math.round(amount * 100),
+        currency: "usd",
+        metadata: {
+          bookingId: booking._id.toString(),
+          userId: user._id.toString(),
+        },
+        automatic_payment_methods: {
+          enabled: true,
+          allow_redirects: "never",
+        },
+      });
+
+      await Payment.create({
+        bookingId: booking._id,
+        userId: user._id,
         amount,
+        currency: "usd",
+        status: "pending",
+        paymentIntentId: paymentIntent.id,
+        method: "stripe",
+      });
+
+      res.status(200).json({
+        clientSecret: paymentIntent.client_secret,
+        message: "Payment initiated",
       });
     }
+    else {
+      const userAmountInCents = Math.round(userAmount * 100);
+      const userId = userData?.id?.toString() as string;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(amount * 100),
-      currency: "usd",
-      metadata: {
-        bookingId: booking._id.toString(),
-        userId: user._id.toString(),
-      },
-      automatic_payment_methods: {
-        enabled: true,
-        allow_redirects: "never",
-      },
-    });
+      if (!userAmount || userAmountInCents < 50) {
+        return res.status(400).json({
+          message: "Amount must be at least $0.50",
+          userAmount,
+        });
+      }
 
-    await Payment.create({
-      bookingId: booking._id,
-      userId: user._id,
-      amount,
-      currency: "usd",
-      status: "pending",
-      paymentIntentId: paymentIntent.id,
-      method: "stripe",
-    });
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: Math.round(userAmount * 100),
+        currency: "usd",
+        metadata: {
+          userRenterId: userId
+        },
+        automatic_payment_methods: {
+          enabled: true,
+          allow_redirects: "never",
+        },
+      });
 
-    res.status(200).json({
-      clientSecret: paymentIntent.client_secret,
-      message: "Payment initiated",
-    });
+      await WalletTransaction.create({
+        userId,
+        type: "credit",
+        amount: userAmount,
+        status: "pending",
+        source: "stripe",
+        paymentIntentId: paymentIntent.id,
+        createdAt: new Date(),
+      });
+
+      res.status(200).json({
+        clientSecret: paymentIntent.client_secret,
+        message: "User payment initiated",
+      });
+    }
   } catch (error) {
     console.error("Payment Intent Error:", error);
     res.status(500).json({ message: "Server error" });
@@ -85,7 +128,7 @@ export const stripeWebhook = async (req: Request, res: Response) => {
 
   const sig = req.headers["stripe-signature"];
   if (!sig) return res.status(400).send("Missing Stripe signature");
-console.log("Webhook Signature:", sig);
+  console.log("Webhook Signature:", sig);
   let event;
   try {
     event = stripe.webhooks.constructEvent(
@@ -99,66 +142,124 @@ console.log("Webhook Signature:", sig);
 
   try {
     const paymentIntent = event.data.object as any;
+
+    const userRenterId = paymentIntent.metadata?.userRenterId;
     const bookingId = paymentIntent.metadata?.bookingId;
 
-    if (!bookingId) return res.status(400).send("Missing booking ID");
+    if (bookingId) {
+      if (!bookingId) return res.status(400).send("Missing booking ID");
 
-    const booking = await Booking.findById(bookingId)
-      .populate("renter")
-      .populate("leaser");
+      const booking = await Booking.findById(bookingId)
+        .populate("renter")
+        .populate("leaser");
 
-    if (!booking) return res.status(404).send("Booking not found");
+      if (!booking) return res.status(404).send("Booking not found");
 
-    const renter = booking.renter as IUser | null;
-    const leaser = booking.leaser as IUser | null;
+      const renter = booking.renter as IUser | null;
+      const leaser = booking.leaser as IUser | null;
 
-    // PAYMENT SUCCESS
-    if (event.type === "payment_intent.succeeded") {
-      console.log("Payment confirmed by Stripe");
+      // PAYMENT SUCCESS
+      if (event.type === "payment_intent.succeeded") {
+        console.log("Payment confirmed by Stripe");
 
-      booking.status = "approved";
-      await booking.save();
+        booking.status = "approved";
+        await booking.save();
 
-      await Payment.findOneAndUpdate(
-        { paymentIntentId: paymentIntent.id },
-        { status: "succeeded" },
-        { new: true }
-      );
+        await Payment.findOneAndUpdate(
+          { paymentIntentId: paymentIntent.id },
+          { status: "succeeded" },
+          { new: true }
+        );
 
-      console.log("updated")
+        console.log("updated")
 
-      // Notifications
-      if (renter?._id) {
-        await sendNotification(
-          renter._id.toString(),
-          "Payment Successful",
-          `Your payment for booking "${booking._id}" was successful.`,
-          { bookingId: booking._id.toString(), type: "payment_succeeded" }
+        // Notifications
+        if (renter?._id) {
+          await sendNotification(
+            renter._id.toString(),
+            "Payment Successful",
+            `Your payment for booking "${booking._id}" was successful.`,
+            { bookingId: booking._id.toString(), type: "payment_succeeded" }
+          );
+        }
+
+        if (leaser?._id) {
+          const renterName = renter?.name || "A user";
+          await sendNotification(
+            leaser._id.toString(),
+            "Payment Succeeded",
+            `${renterName} completed payment for booking.`,
+            { bookingId: booking._id.toString(), type: "payment_succeeded" }
+          );
+        }
+      }
+
+      //  PAYMENT FAILED
+      else if (event.type === "payment_intent.payment_failed") {
+        console.log(" Payment failed");
+
+        await Payment.findOneAndUpdate(
+          { paymentIntentId: paymentIntent.id },
+          { status: "failed" }
         );
       }
 
-      if (leaser?._id) {
-        const renterName = renter?.name || "A user";
-        await sendNotification(
-          leaser._id.toString(),
-          "Payment Succeeded",
-          `${renterName} completed payment for booking.`,
-          { bookingId: booking._id.toString(), type: "payment_succeeded" }
+      res.json({ received: true });
+    }
+
+    else {
+      if (!userRenterId) return res.status(400).send("Missing User ID");
+
+      const walletData = await WalletTransaction.find(
+        {
+          userId: userRenterId,
+          paymentIntentId: paymentIntent.id
+        }
+      )
+
+      if (!walletData) return res.status(404).send("Wallet data not found");
+
+      // PAYMENT SUCCESS
+      if (event.type === "payment_intent.succeeded") {
+        console.log("Payment confirmed by Stripe");
+
+        await WalletTransaction.findOneAndUpdate(
+          {
+            userId: userRenterId,
+            paymentIntentId: paymentIntent.id
+          },
+          { status: "succeeded" },
+          { new: true }
+        );
+
+        console.log("updated")
+
+        // Notifications
+        if (userRenterId) {
+          await sendNotification(
+            userRenterId,
+            "Payment Successful",
+            `Your payment was successful.`,
+            { userId: userRenterId, type: "payment_succeeded" }
+          );
+        }
+      }
+
+      //  PAYMENT FAILED
+      else if (event.type === "payment_intent.payment_failed") {
+        console.log(" Payment failed");
+
+        await WalletTransaction.findOneAndUpdate(
+          {
+            userId: userRenterId,
+            paymentIntentId: paymentIntent.id
+          },
+          { status: "failed" }
         );
       }
+
+      res.json({ received: true });
     }
-
-    //  PAYMENT FAILED
-    else if (event.type === "payment_intent.payment_failed") {
-      console.log(" Payment failed");
-
-      await Payment.findOneAndUpdate(
-        { paymentIntentId: paymentIntent.id },
-        { status: "failed" }
-      );
-    }
-
-    res.json({ received: true });
   } catch (err) {
     console.error("Webhook Processing Error:", err);
     res.status(500).send("Webhook processing error");
