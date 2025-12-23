bef43331bba1ec1185cb7c1f345cac66079b1cf6
umerwe <uemyy1@gmail.com>
2025-12-23 09:49:51 +0500

    updated

--- src/models/walletTransaction.model.ts
+++ src/models/walletTransaction.model.ts
@@ -6,7 +6,8 @@ export interface IWalletTransaction extends Document {
   amount: number;
   source: "stripe" | "booking" | "refund" | "withdraw" | string;
   bankAccountId: mongoose.Types.ObjectId; // Refers to user's bank account
-  status: "pending" | "approved" | "rejected";
+  status: "pending" | "succeeded" | "failed";
+  paymentIntentId: string;
   description?: string;
   createdAt: Date;
   requestedAt: Date;
@@ -15,11 +16,13 @@ export interface IWalletTransaction extends Document {
 
 const WalletTransactionSchema = new Schema<IWalletTransaction>({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  type: { type: String, enum: ["credit", "debit", "withdraw"], required: true },
+  type: { type: String },
+  // type: { type: String, enum: ["credit", "debit", "withdraw"] },
   amount: { type: Number, required: true },
   bankAccountId: { type: Schema.Types.ObjectId, required: false },
-  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+  status: { type: String, enum: ["pending", "succeeded", "failed"], default: "pending" },
   source: { type: String, required: true },
+  paymentIntentId: { type: String, required: true },
   description: { type: String },
   createdAt: { type: Date, default: Date.now },
   requestedAt: { type: Date, default: Date.now },
--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,15 +1,10 @@
 import express from "express";
 import { createBookingPayment, stripeWebhook } from "../controllers/payment.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = express.Router();
-
-// router.post(
-//   "/stripe/webhook",
-//   express.raw({ type: "application/json" }),
-//   stripeWebhook as express.RequestHandler
-// );
-
+const useAuth = authMiddleware as any;
 //Create Payment Intent for a Booking
-router.post("/stripe/intent", createBookingPayment as express.RequestHandler);
+router.post("/stripe/intent", useAuth, createBookingPayment as express.RequestHandler);
 
 export default router;
